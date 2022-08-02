02c5f1989393236fef9cdd034dd3491e852b5dde
yokgs <yazid.slila01@gmail.com>
2022-08-02 16:58:36 +0100

    fix : user identification

--- src/middleware/decodeuser.middleware.ts
+++ src/middleware/decodeuser.middleware.ts
@@ -6,7 +6,7 @@ import { IPayload } from '../types/jwtpayload.interface';
 
 export const decodeUser = async (req: Request, res: Response, next: NextFunction) => {
 
-	const $token = req.headers['Authorization'].toString();
+	const $token = req.headers['Authorization']?.toString();
 	if ($token) {
 		try {
 			req.currentUser = jwt.verify(
